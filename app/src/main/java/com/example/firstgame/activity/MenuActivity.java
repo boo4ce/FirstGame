6b905fc71dbc7c1bb03119ecfadbf87971446a00
boo4ce <69792699+boo4ce@users.noreply.github.com>
2021-02-19 21:37:48 +0700

    Add press animation

--- app/src/main/java/com/example/firstgame/activity/MenuActivity.java
+++ app/src/main/java/com/example/firstgame/activity/MenuActivity.java
@@ -3,6 +3,7 @@ package com.example.firstgame.activity;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,10 +11,12 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
+
 import com.example.firstgame.R;
 
 public class MenuActivity extends Activity {
-    @SuppressLint("ResourceType")
+    @SuppressLint({"ResourceType", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -22,40 +25,65 @@ public class MenuActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         this.setContentView(R.layout.menu_activity);
-        Button button = (Button) findViewById(R.id.startGame);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        final Button[] button = {findViewById(R.id.continues), findViewById(R.id.startGame),
+                findViewById(R.id.ball), findViewById(R.id.setting), findViewById(R.id.quit)};
+
+        button[0].setClickable(false);
+        button[0].setAlpha(0.3F);
+
+        //set touch listener new game
+        button[1].setOnTouchListener((v, event) -> {
+            setAnimClick(button[1], event);
+            if(event.getAction() == MotionEvent.ACTION_UP) {
                 Intent intent = new Intent(getApplicationContext(), SelectLevelActivity.class);
                 startActivity(intent);
             }
+            return true;
         });
 
-        button = findViewById(R.id.quit);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // ball
+        button[2].setOnTouchListener((v, event) -> {
+            setAnimClick(button[2], event);
+            return true;
+        });
+
+        // setting
+        button[3].setOnTouchListener((v, event) -> {
+            setAnimClick(button[3], event);
+            return true;
+        });
+
+        // quit
+        button[4].setOnTouchListener((v, event) -> {
+            setAnimClick(button[4], event);
+            if(event.getAction() == MotionEvent.ACTION_UP)
                 MenuActivity.this.finish();
-            }
+            return true;
         });
+        //set animation
+    }
 
-        button = findViewById(R.id.continues);
-        button.setClickable(false);
-        button.setAlpha(0.3F);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Pause");
+    }
 
-        button = findViewById(R.id.setting);
-        button.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Button button1 = findViewById(R.id.setting);
-                if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                    button1.setAlpha(0.3F);
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP)
-                    button1.setAlpha(1);
-                return true;
-            }
-        });
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.finishAndRemoveTask();
+        System.out.println("Destroy");
+    }
+
+    private void setAnimClick(Button button, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            button.setAlpha(0.3F);
+        }
+        else if(event.getAction() == MotionEvent.ACTION_UP)
+            button.setAlpha(1);
     }
 
 }
--- app/src/main/java/com/example/firstgame/activity/SelectLevelActivity.java
+++ app/src/main/java/com/example/firstgame/activity/SelectLevelActivity.java
@@ -1,30 +1,36 @@
 package com.example.firstgame.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.firstgame.R;
 import com.example.firstgame.attributes.Level;
 
 public class SelectLevelActivity extends Activity {
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle saveInstanceBundle) {
         super.onCreate(saveInstanceBundle);
         this.setContentView(R.layout.select_level);
 
-        int[] selected_level = {R.id.slow, R.id.normal, R.id.fast};
+        final int[] selected_level = {R.id.slow, R.id.normal, R.id.fast};
         int[] level = {Level.SLOW, Level.NORMAL, Level.FAST};
 
         for(int i = 0; i < 3; i++) {
             final int j = i;
-            findViewById(selected_level[i]).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            findViewById(selected_level[i]).setOnTouchListener((v, event) -> {
+                setAnimClick(findViewById(selected_level[j]), event);
+                if(event.getAction() == MotionEvent.ACTION_UP) {
                     openNewActivity(level[j]);
                     SelectLevelActivity.this.finish();
                 }
+                return true;
             });
         }
         
@@ -40,5 +46,14 @@ public class SelectLevelActivity extends Activity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         Level.setLevel(level);
         startActivity(intent);
+    }
+
+    private void setAnimClick(Button button, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            button.setAlpha(0.3F);
+        }
+        else if(event.getAction() == MotionEvent.ACTION_UP) {
+            button.setAlpha(1);
+        }
     }
 }
