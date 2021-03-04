260f21e5e6a84a0782ad96603694b34f8a6cc47d
Hello <hello@gmail.com>
2021-03-04 22:29:19 +0700

    Fix read and write filesave

--- app/src/main/java/com/example/firstgame/activity/MainActivity.java
+++ app/src/main/java/com/example/firstgame/activity/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.firstgame.activity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 
-import com.example.firstgame.controller.GameController;
 import com.example.firstgame.controller.IOFile;
 import com.example.firstgame.view.GameView;
-
-import java.io.File;
 
 public class MainActivity extends FullScreenActivity {
     private IOFile ioFile;
@@ -30,6 +23,8 @@ public class MainActivity extends FullScreenActivity {
         ioFile = new IOFile();
         gameView = new GameView(this);
 
+        // load filesave
+        // if don't have filesave, create new game
         Intent intent = getIntent();
         if(intent.getIntExtra("flag", 0) == MenuActivity.START_WITH_PREVIOUS_GAME) {
             try {
@@ -73,6 +68,7 @@ public class MainActivity extends FullScreenActivity {
     }
 
     private void save() {
+        // write filesave
         if(gameView.filesave().equals("")) {
             ioFile.delete();
         }
